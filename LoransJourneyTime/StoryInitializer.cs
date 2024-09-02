42e8c46bac75155cf4b97285ad75440560328b87
Michel Escaravage <108881977+MichelEscaravage@users.noreply.github.com>
2024-09-02 10:40:40 +0200

    Made a story initializer

--- LoransJourneyTime/StoryInitializer.cs
+++ LoransJourneyTime/StoryInitializer.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoransJourneyTime
+{
+    internal class StoryInitializer
+    {
+        public static void Choice()
+        {
+
+        }
+    }
+
+    class Scene
+    {
+        public string Text { get; }
+        public List<Choice> Choices { get; }
+
+        public Scene(string text, List<Choice> choices)
+        {
+            Text = text;
+            Choices = choices;
+        }
+    }
+
+    class Choice
+    {
+        public string Text { get; }
+        public float NextScene { get; }
+
+        public Choice(string option, float nextScene)
+        {
+            Text = option;
+            NextScene = nextScene;
+        }
+    }
+}
